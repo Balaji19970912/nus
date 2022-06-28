98a0de5e686b716c1571a7938904ac872ebdbded
Balaji S Tanganoor <36842756+Balaji19970912@users.noreply.github.com>
2022-06-28 10:46:52 +0530

    Add files via upload

---
+++ signin/login.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../img/social-square-n-blue.png">
+    <title>NUS FRS System</title>
+    <!-- <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/login.css"> -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" type="text/css" href="css/login.css">
+</head>
+<body>
+    <nav>
+    </nav>
+    <header>
+        
+    </header>
+    <section class="sectionContainer">
+    <a href="#"><img src="../img/NUS Logo NUSdirect 2020_W.png" alt="NUS Consulting Group Logo" height=40px></a>
+        <div class="container1">
+            <h2>Welcome to NUS FRS System.</h2>
+            <p>
+            NUS Consulting Group’s secure energy information system (EiS) for clients seeking a new way to access and use their energy data to achieve their cost management and sustainability objectives.
+            </p>
+        </div>
+        <br>
+        <p>...........................................</p>
+        <div class="container2">
+            <h2>Enter your login details.</h2>
+            <p>
+            NUS Consulting is implementing enhanced security measures, and accordingly the login procedure has now been updated.
+            </p>
+            <form action="" class="loginForm">
+                <label for="username">Username</label>
+                <input type="email" name="useremail" placeholder="name@address.com"><br>
+                <label for="password">Password</label>
+                <input type="password" name="userpassword" placeholder="password"><br>
+                <input type="submit" value="Sign in" class="signIn"><br><br>
+                <a href="" class="fogotPassword">Forgot Username/Password ?</a>
+            </form>
+        </div>
+    </section>
+    <footer>
+        <ul>
+            <li><a href="">&#x40; NUS Consulting 2022</a></li>
+            <li><a href="">Terms of Use</a></li>
+            <li><a href="">Site Information</a></li>
+            <li><a href="">Cookie Policy</a></li>
+            <li><a href="">NUSdirect 4.0</a></li>
+        </ul>
+    </footer>
+</body>
+</html>
